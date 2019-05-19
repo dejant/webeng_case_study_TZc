7018ba785176019f5c940eba6a2f59857e914780
unknown <schmip16@students.zhaw.ch>
2019-05-19 15:06:47 +0200

    teams mit Suche und Listenansicht angepasst

--- scripts/app.js
+++ scripts/app.js
@@ -36,3 +36,25 @@ function searchFilter() {
     }
   }
 }
+
+function searchFilterTeams() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("teamsTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
