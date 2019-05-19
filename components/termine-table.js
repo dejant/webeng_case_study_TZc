43a9700fe1f735875f7ac5f04e0b0926c063f204
unknown <schmip16@students.zhaw.ch>
2019-05-19 20:48:12 +0200

    termine in Listenansicht und Suchfunktion

--- components/termine-table.js
+++ components/termine-table.js
@@ -1,23 +1,21 @@
 // Instanzen
 //
 var app = new Vue({
-    el: '#teamsTable',
+    el: '#termineTable',
     data: {
-        teams: [
-            { id: 1, name: "Torschützen", anzahl:'24'},
-            { id: 2, name: "Wegen dem Bier", anzahl:'12'},
-            { id: 3, name: "Nichtskönner", anzahl:'13'},
-            { id: 4, name: "1. Liga", anzahl:'18'},
-            { id: 5, name: "GCZ Rasenblockierer", anzahl:'19'},
-            { id: 6, name: "Feierabendrunde", anzahl:'11'}
+        termine: [
+            { id: 1, name: "1. Liga Spiel", beschreibung: "Kommt und heizt unseren Jungs ein.", start:"20.05.2019", ende:"20.05.2019", ort:"47.500897, 8.717852", map:"https://goo.gl/maps/8ytAGhb7WFebiqAY8"},
+            { id: 2, name: "2. Liga Spiel", beschreibung: "Kommt und heizt unseren Jungs ein.", start:"22.05.2019", ende:"23.05.2019", ort:"47.500897, 8.717852", map:"https://goo.gl/maps/8ytAGhb7WFebiqAY8"},
+            { id: 3, name: "Feierabendrunde", beschreibung: "Spielen zum Spass", start:"23.05.2019", ende:"24.05.2019", ort:"47.500897, 8.717852", map:"https://goo.gl/maps/8ytAGhb7WFebiqAY8"},
+            { id: 4, name: "Trainingslager", beschreibung: "Sommer, Sonne, Fussball", start:"20.07.2019", ende:"22.07.2019", ort:"47.500897, 8.717852", map:"https://goo.gl/maps/8ytAGhb7WFebiqAY8"}
         ]
     },
     computed: {
         "columns": function columns() {
-            if (this.teams.length == 0) {
+            if (this.termine.length == 0) {
                 return [];
             }
-            return Object.keys(this.teams[0])
+            return Object.keys(this.termine[0])
         }
     },
     methods: {
@@ -31,7 +29,7 @@ var app = new Vue({
 
             var ascending = this.ascending;
 
-            this.teams.sort(function (a, b) {
+            this.termine.sort(function (a, b) {
                 if (a[col] > b[col]) {
                     return ascending ? 1 : -1
                 } else if (a[col] < b[col]) {
