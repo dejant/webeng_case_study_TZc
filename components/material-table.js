5bac8738af6f732690c880d19a9adf9f08fab0f4
unknown <schmip16@students.zhaw.ch>
2019-05-19 20:23:09 +0200

    Materialtabelle

---
+++ components/material-table.js
@@ -0,0 +1,43 @@
+// Instanzen
+//
+var app = new Vue({
+    el: '#materialTable',
+    data: {
+        teams: [
+            { id: 1, name: "Fussbälle", beschreibung: "Bälle die mit dem Fuss getreten werden.", anzahl:'24'},
+            { id: 2, name: "Tennisbälle", beschreibung: "Werden bei uns selten gebraucht", anzahl:'10'},
+            { id: 3, name: "Medinzinbälle", beschreibung: "Für das schwere Training", anzahl:'8'},
+            { id: 4, name: "Trickots", beschreibung: "Um unsere Mannschaft zu erkennen.", anzahl:'30'}
+        ]
+    },
+    computed: {
+        "columns": function columns() {
+            if (this.teams.length == 0) {
+                return [];
+            }
+            return Object.keys(this.teams[0])
+        }
+    },
+    methods: {
+        "sortTable": function sortTable(col) {
+            if (this.sortColumn === col) {
+                this.ascending = !this.ascending;
+            } else {
+                this.ascending = true;
+                this.sortColumn = col;
+            }
+
+            var ascending = this.ascending;
+
+            this.teams.sort(function (a, b) {
+                if (a[col] > b[col]) {
+                    return ascending ? 1 : -1
+                } else if (a[col] < b[col]) {
+                    return ascending ? -1 : 1
+                }
+                return 0;
+            })
+        }
+        
+    }
+})
